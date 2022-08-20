3fe396c9a82644ade7c1abff98e658311a4d51e5
Miguel Desimone <miguel.desimone98@gmail.com>
2022-08-20 10:59:00 +0200

    ejercicio I corregido

--- week-1/InClass/exercise-I.js
+++ week-1/InClass/exercise-I.js
@@ -3,9 +3,9 @@ function dateBirth (age) {
 }
 
 function greeting(name, age) {
-  var date = dateBirth(24);
-  var name = "Miguel";
- return "My name is " + name + " and I was born in the year " + date;
+  const date = dateBirth(24);
+  const myName = "Miguel";
+ return "My name is " + myName + " and I was born in the year " + date;
 }
 
 console.log (greeting());
